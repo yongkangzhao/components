1e9cc766ae4010d1e744f9d185f36df98187d787
Yongkang Zhao <cookieyong@gmail.com>
2020-11-14 20:08:31 -0800

    updated the approvalCard component to accept props.children as it's content

--- src/index.js
+++ src/index.js
@@ -8,10 +8,15 @@ import ApprovalCard from './ApprovalCard'
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" comment="nice blog post!" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Alex" timeAgo="Today at 2:45 AM" comment="very nice blog post!" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Jane" timeAgo="Yesterday at 8:00 AM" comment="nice post!" avatar={faker.image.avatar()}/>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" comment="nice blog post!" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 2:45 AM" comment="very nice blog post!" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Yesterday at 8:00 AM" comment="nice post!" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
         </div>
         
     );
