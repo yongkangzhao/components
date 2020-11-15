963589dc647e197c40aa389713fd7d9d12a32402
Yongkang Zhao <cookieyong@gmail.com>
2020-11-14 19:58:52 -0800

    made some changes to make the commentdetail component to be able to accept more props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import faker from 'faker';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam"/>
-            <CommentDetail author="Alex"/>
-            <CommentDetail author="Jane"/>
+            <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" comment="nice blog post!" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Alex" timeAgo="Today at 2:45 AM" comment="very nice blog post!" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Jane" timeAgo="Yesterday at 8:00 AM" comment="nice post!" avatar={faker.image.avatar()}/>
         </div>
     );
 };
