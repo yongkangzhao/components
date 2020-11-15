ea42b7409edb8a1a3f288f73b5defaeb2d5a0836
Yongkang Zhao <cookieyong@gmail.com>
2020-11-14 20:01:49 -0800

    showing the basic approval card inside html

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard'
+
 
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard />
             <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" comment="nice blog post!" avatar={faker.image.avatar()}/>
             <CommentDetail author="Alex" timeAgo="Today at 2:45 AM" comment="very nice blog post!" avatar={faker.image.avatar()}/>
             <CommentDetail author="Jane" timeAgo="Yesterday at 8:00 AM" comment="nice post!" avatar={faker.image.avatar()}/>
         </div>
+        
     );
 };
 
